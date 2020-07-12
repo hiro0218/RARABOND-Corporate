c6386f9185eca1fdc8797cba6254108ee6688af6
hiro0218 <hiro0218@gmail.com>
2020-07-13 00:49:08 +0900

    fix: page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: `なちゅらる | ${process.env.npm_package_description}`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
