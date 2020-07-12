5a5e9f93b037ceead1d1bd2e178d134afdb99dc4
hiro0218 <hiro0218@gmail.com>
2020-07-12 23:43:37 +0900

    add: footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="pg-footer">
+    <small class="pg-footer-copyright">
+      ©ララボンド All Rights Reserved.
+    </small>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+};
+</script>
+
+<style lang="scss">
+.pg-footer {
+  padding: 40px 0;
+  border-top: 1px solid #eee;
+  text-align: center;
+}
+
+.pg-footer-copyright {
+  font-size: 1rem;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <nuxt />
+    <TheFooter />
   </div>
 </template>
+
+<script>
+import TheFooter from '~/components/TheFooter.vue';
+
+export default {
+  components: {
+    TheFooter,
+  },
+};
+</script>
 
 <style></style>
