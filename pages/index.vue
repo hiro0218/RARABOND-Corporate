701e5c67f55556b4cc5d76cf60813a39c54d62ac
hiro <hiro0218@gmail.com>
2020-08-30 13:00:59 +0900

    replace: map

--- pages/index.vue
+++ pages/index.vue
@@ -108,7 +108,7 @@
       <h2 class="section__title">アクセス</h2>
       <div>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1693.2905119254954!2d131.4329572581108!3d31.917979271985867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3538b75ba150fd3f%3A0xf489b40a624e2f9a!2z44CSODgwLTA4Nzkg5a6u5bSO55yM5a6u5bSO5biC5a6u5bSO6aeF5p2x77yT5LiB55uu77yS4oiS77yS!5e0!3m2!1sja!2sjp!4v1597025984918!5m2!1sja!2sjp"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13546.324429991124!2d131.4340516!3d31.917977!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbd06f68306503ab3!2z44Gq44Gh44KF44KJ44KL!5e0!3m2!1sja!2sjp!4v1598759871203!5m2!1sja!2sjp"
           width="100%"
           height="450"
           frameborder="0"
